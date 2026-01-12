1baa3b1e70229bc2af2ebe9291d2247b8d02c037
devmatias13 <saraviamatias678@gmail.com>
2026-01-12 10:00:35 -0300

    Ahora se puede editar los datos del cliente.

--- Client/src/pages/Clients.jsx
+++ Client/src/pages/Clients.jsx
@@ -121,7 +121,8 @@ const Clients = () => {
                   <MoreVertical size={18} />
                 </button>
                 {openMenu === client.id && (
-                  <div data-menu className="absolute right-0 mt-2 w-36 bg-gray-900 border border-gray-800 rounded shadow-lg z-50">
+                  <div data-menu className="absolute right-0 mt-2 w-40 bg-gray-900 border border-gray-800 rounded shadow-lg z-50">
+                    <Link to={`/clients/new?id=${client.id}`} className="block w-full text-left px-4 py-2 hover:bg-gray-800 hover:text-white text-gray-200">Editar</Link>
                     <button onClick={() => deleteClient(client.id)} className="w-full text-left px-4 py-2 hover:bg-red-600 hover:text-white text-red-400">Eliminar</button>
                   </div>
                 )}
@@ -194,7 +195,8 @@ const Clients = () => {
                         <MoreVertical size={18} />
                       </button>
                       {openMenu === client.id && (
-                        <div data-menu className="absolute right-0 mt-2 w-36 bg-gray-900 border border-gray-800 rounded shadow-lg z-50">
+                        <div data-menu className="absolute right-0 mt-2 w-40 bg-gray-900 border border-gray-800 rounded shadow-lg z-50">
+                          <Link to={`/clients/new?id=${client.id}`} className="block w-full text-left px-4 py-2 hover:bg-gray-800 hover:text-white text-gray-200">Editar</Link>
                           <button onClick={() => deleteClient(client.id)} className="w-full text-left px-4 py-2 hover:bg-red-600 hover:text-white text-red-400">Eliminar</button>
                         </div>
                       )}
